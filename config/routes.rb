cbfd94fdad5ee9f5aaa0caf35e98404822e5ce5d
github username <github email address>
2019-09-09 21:11:54 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,22 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # STUDENT PATHS
-  get '/students', to: 'students#index', as: 'students'
-  get '/students/new', to: 'students#new', as: 'new_student'
-  post '/students', to: 'students#create'
-  get '/students/:id', to: 'students#show', as: 'student'
-  get '/students/:id/edit', to: 'students#edit', as: 'edit_student'
-  patch '/students/:id', to: 'students#update'
+  # get '/students', to: 'students#index', as: 'students'
+  # get '/students/new', to: 'students#new', as: 'new_student'
+  # post '/students', to: 'students#create'
+  # get '/students/:id', to: 'students#show', as: 'student'
+  # get '/students/:id/edit', to: 'students#edit', as: 'edit_student'
+  # patch '/students/:id', to: 'students#update'
+  resources :students, only: [:index, :new, :create, :show, :edit, :update]
 
   # SCHOOL_CLASS PATHS
-  get '/school_classes', to: 'school_classes#index', as: 'school_classes'
-  get '/school_classes/new', to: 'school_classes#new', as: 'new_school_class'
-  post '/school_classes', to: 'school_classes#create'
-  get '/school_classes/:id', to: 'school_classes#show', as: 'school_class'
-  get '/school_classes/:id/edit', to: 'school_classes#edit', as: 'edit_school_class'
-  patch '/school_classes/:id', to: 'school_classes#update'
+  # get '/school_classes', to: 'school_classes#index', as: 'school_classes'
+  # get '/school_classes/new', to: 'school_classes#new', as: 'new_school_class'
+  # post '/school_classes', to: 'school_classes#create'
+  # get '/school_classes/:id', to: 'school_classes#show', as: 'school_class'
+  # get '/school_classes/:id/edit', to: 'school_classes#edit', as: 'edit_school_class'
+  # patch '/school_classes/:id', to: 'school_classes#update'
+  resources :school_classes, only: [:index, :new, :create, :show, :edit, :update]
 
 
 end
